2f854c8df9561a3ae0b8c1c5e2aaed04546846a4
ksande91 <krisande91@gmail.com>
2024-06-19 14:46:29 +0200

    Initial commit

---
+++ lua/vim-options.lua
@@ -0,0 +1,12 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+vim.wo.number = true
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
